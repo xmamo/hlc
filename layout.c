14bccd3038d0a073ac6c274f6b04e701001f7eba
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2024-06-20 08:33:32 +0200

    Optimize `hlc_layout_add`

--- layout.c
+++ layout.c
@@ -9,11 +9,7 @@ size_t hlc_layout_add(hlc_Layout* layout, hlc_Layout member) {
 
   size_t member_size = member.size;
   size_t member_alignment = member.alignment != 0 ? member.alignment : 1;
-  size_t member_offset = (layout->size / member_alignment) * member_alignment;
-
-  while (member_offset < layout->size) {
-    member_offset += member_alignment;
-  }
+  size_t member_offset = (layout->size + (member_alignment - 1)) / member_alignment * member_alignment;
 
   layout->size = member_offset + member_size;
 
